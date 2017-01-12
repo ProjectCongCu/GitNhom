5dad86b61f473ce5767b3db3ec4f73ef4a08f3a0
Cuong <pholaodong95@gmail.com>
2017-01-12 20:53:32 +0700

    Toi la Cuong

---
+++ Hinh.java
@@ -0,0 +1,17 @@
+package com.example.administrator.ssd;
+
+import android.widget.ImageView;
+
+/**
+ * Created by Administrator on 6/1/2016.
+ */
+public class Hinh {
+    ImageView hinh;
+    int open1;
+    int open2;
+    Hinh(){
+        hinh=null;
+        open1=0;
+        open2=0;
+    }
+}
